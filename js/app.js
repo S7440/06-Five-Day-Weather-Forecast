3d7a0ed7c31ecef3bf2781e209278388c5693189
Skylar Fynboh <114550923+S7440@users.noreply.github.com>
2022-12-04 01:50:10 -0600

    Ask BCS

--- js/app.js
+++ js/app.js
@@ -12,25 +12,43 @@ var currentHeaderEl = document.querySelector('#current-Header');
   var currentWindSpeedEl = document.querySelector('#current-wind-speed');
 var currentIconEl = document.querySelector('#current-icon');
 
-
+function displayResults() {
 //projected card variables that will run 5 cycles and print off 5 data sets.
+var resultsEl = document.querySelector('#results');
 var projectedCard = document.createElement('section');
-projectedCard.appendChild('#results');
-projectedCard.setAttribute('#projected-card');
   var projectedContainerEl = document.createElement('div');
-  projectedContainerEl.appendChild('#projected-card');
-  projectedContainerEl.setAttribute('#projected-container');
-    var projectedDateEl = document.querySelector('div');
-    projectedDateEl.appendChild('#projected-container');
-    projectedDateEl.setAttribute('#projected-date')
-    var projectedIconEl = document.querySelector('#projected-icon');
-  var projectedConditionsEl = document.querySelector('#projected-conditions');
-    var projectedTempEl = document.querySelector('#projected-temp');
+    var projectedDateEl = document.createElement('span');
+    var projectedIconEl = document.createElement('span');
+  var projectedConditionsEl = document.createElement('div');
+    var projectedTempEl = document.createElement('span');
     var projectedHumidityEl = document.querySelector('#projected-humidity');
-    var projectedWindSpeedEl = document.querySelector('#projected-wind-speed')
+    var projectedWindSpeedEl = document.querySelector('#projected-wind-speed');
 
+console.log(temp);
+return;
+    resultsEl.appendChild(projectedCard);
+    projectedCard.setAttribute('#projected-card');    
+    projectedCard.appendChild(projectedContainerEl);
+    projectedContainerEl.setAttribute('#projected-container');
+    projectedContainerEl.appendChild('projectedDateEl');
+    projectedDateEl.appendChild(cityDate);
+    projectedDateEl.setAttribute('#projected-date');
+    projectedContainerEl.appendChild(projectedIconEl);
+    projectedIconEl.setAttribute('#projected-icon');
+    projectedIconEl.appendChild(weatherIcon);
+    projectedCard.appendChild(projectedConditionsEl);
+    projectedConditionsEl.setAttribute('#projected-conditions');
+    projectedConditionsEl.appendChild(projectedTempEl);
+    projectedTempEl.setAttribute('#projected-temp');
+    projectedTempEl.appendChild(temp);
+    projectedConditionsEl.appendChild(projectedHumidityEl);
+    projectedHumidityEl.setAttribute('#projected-humidity');
+    projectedHumidityEl.appendChild(humidity);
+    projectedConditionsEl.appendChild(projectedWindSpeedEl);
+    projectedWindSpeedEl.setAttribute('#projected-wind-speed');
+    projectedWindSpeedEl.appendChild(windSpeed);
 
-
+};
 
 
 var inputSubmitHandler = function (event) {
@@ -75,27 +93,22 @@ var getForecast = function (lat, lon) {
     lon +
     "&units=imperial&appid=5eadcacf0e30dbacf32a851c3ca447bb";
 
-  fetch(apiTwoUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        currentForecast.textContent = "";
-        var cityName = data.city.name;
-        var cityDate = data.list[3].dt_txt;
-        var weatherIcon = data.list[3].weather[0].icon;
-        var humidity = data.list[3].main.humidity;
-        var temp = data.list[3].main.temp;
-        var windSpeed = data.list[3].wind.speed;
+    fetch(apiTwoUrl).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          displayResults();
+          var cityName = data.city.name;
+          var cityDate = data.list[3].dt_txt;
+          var weatherIcon = data.list[3].weather[0].icon;
+          var humidity = data.list[3].main.humidity;
+          var temp = data.list[3].main.temp;
+          var windSpeed = data.list[3].wind.speed;
+  
 
-        for (var i = 0; i <5; i ++) {
-
- 
-          
-        }
-      });
-    }
-  });
-};
+          })
+        };
+  });};
 
 submission.addEventListener("click", inputSubmitHandler);
 
